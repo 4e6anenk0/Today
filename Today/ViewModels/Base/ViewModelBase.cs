bc7752dbd59e347ff017dc3970ce21fcb318fc9b
4e6anenk0 <4e6anenk0@gmail.com>
2020-12-18 00:49:52 +0200

    Creating the ViewModelBase class

---
+++ Today/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Today.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Базовая реализация метода, который необходим для мониторинга изменений в свойствах
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        // Задача этого метода состоит в обновлении свойства новым значением. Необходимо для разрешения 
+        // кольцевых обновлений свойст для избежания переполнения стека. Т.е. мы производим вызов OnPropertyChanged
+        // только в том случае, когда значение свойство отличается
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
