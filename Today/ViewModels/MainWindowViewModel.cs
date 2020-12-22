e1f4c3457c047ff4502a3dfa84f9cb695d86befc
4e6anenk0 <4e6anenk0@gmail.com>
2020-12-22 06:10:26 +0200

    Added simplest window close command

--- Today/ViewModels/MainWindowViewModel.cs
+++ Today/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Today.Infrastructure.Commands;
 using Today.ViewModels.Base;
 
 namespace Today.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-        
+        #region Команды
+
+        #region
+        public ICommand CloseAppCommand { get; }
+
+        private void OnCloseAppCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private bool CanCloseAppCommandExecuted(object p) => true;
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseAppCommand = new LambdaCommand(OnCloseAppCommandExecuted, CanCloseAppCommandExecuted);
+
+            #endregion
+        }
+
     }
 }
