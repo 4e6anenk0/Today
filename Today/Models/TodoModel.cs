75bb1efe00d00d9aa3693f515beabf6108de7c92
4e6anenk0 <4e6anenk0@gmail.com>
2020-12-22 22:19:58 +0200

    Completed the basic description of the model notes and added a namespace for our models to the main markup

--- Today/Models/TodoModel.cs
+++ Today/Models/TodoModel.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Today.Models
 {
-    class TodoModel
+    internal class TodoModel
     {
         private bool _isDone;
         private string _text;
+        private string _label;
+        private DateTime _endData;
 
         public DateTime DataCreation { get; set; } = DateTime.Now;
 
@@ -23,6 +25,18 @@ namespace Today.Models
         {
             get { return _text; }
             set { _text = value; }
+        }
+
+        public string label
+        {
+            get { return _label; }
+            set { _label = value; }
+        }
+
+        public DateTime endData
+        {
+            get { return _endData; }
+            set { _endData = value; }
         }
     }
 }
